9b10542699c98cd77b0212a46b1c945e344722a1
Surya6704 <surya4balram@outlook.com>
2026-03-24 13:45:24 -0700

    calls for functions in agent.py file

--- src/agent.py
+++ src/agent.py
@@ -1,8 +1,21 @@
-##order.py
-#acceptOrder
-#markComplete
-#orderPickedUp
+# Authors: Surya Balram
 
-##Notification.py
+import notification     #importing member functions from notification.py
+import order            #importing member functions from order.py
+
+## Notification.py
 #viewNotification
 #sendNotification
+
+
+## order.py
+#acceptOrder
+def agent_accept_order(self):
+    message = accept_order(self)    #calls accept_order function from order.py and stores in message variable
+
+#markComplete
+def agent_mark_complete(self):
+    message = mark_complete(self)   #calls mark_complete function from order.py and stores in message variable
+
+#orderPickedUp / TBD
+
